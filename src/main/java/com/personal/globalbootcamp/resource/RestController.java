6f532fc61b1a7b42920b4f2a4d0df322021bad2d
saintGGWP <gromchenko.work@gmail.com>
2022-11-18 17:43:05 +0300

    Small refactoring

--- src/main/java/com/personal/globalbootcamp/resource/RestController.java
+++ src/main/java/com/personal/globalbootcamp/resource/RestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @org.springframework.web.bind.annotation.RestController
-// TODO refactor to use OpenAPI 3.0
+// TODO refactor to use OpenAPI 3.0.
 public class RestController {
 
   @PostMapping("/cars")
