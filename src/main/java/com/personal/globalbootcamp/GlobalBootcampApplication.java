a1316af77c9a5130204bb0ddb25f6ea844c19852
saintGGWP <gromchenko.work@gmail.com>
2022-11-18 13:17:10 +0300

    Init commit

---
+++ src/main/java/com/personal/globalbootcamp/GlobalBootcampApplication.java
@@ -0,0 +1,12 @@
+package com.personal.globalbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalBootcampApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GlobalBootcampApplication.class, args);
+  }
+}
