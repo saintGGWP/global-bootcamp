202f6279361175d4c8aa0c152b2c973ae993e2f4
saintGGWP <gromchenko.work@gmail.com>
2022-11-18 14:47:30 +0300

    Add controller Small refactoring

---
+++ src/main/java/com/personal/globalbootcamp/model/Car.java
@@ -0,0 +1,5 @@
+package com.personal.globalbootcamp.model;
+
+public class Car {
+
+}
---
+++ src/main/java/com/personal/globalbootcamp/resource/RestController.java
@@ -0,0 +1,23 @@
+package com.personal.globalbootcamp.resource;
+
+import com.personal.globalbootcamp.model.Car;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@org.springframework.web.bind.annotation.RestController
+// TODO refactor to use OpenAPI 3.0
+public class RestController {
+
+  @PostMapping("/cars")
+  public void addCar() {
+
+  }
+
+  @GetMapping("/cars")
+  public List<Car> getCars() {
+    return new ArrayList<>();
+  }
+
+}
